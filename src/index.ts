9d99d5b73fdb3396beac212b8f05c5a3085e85f7
ajuala <kamalustudio@yandex.com>
2025-01-06 00:14:47 +0100

    test secret 3

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,17 @@ import { Client } from 'pg'
 const app = new Hono()
 
 app.get('/',  async (c) => {
-  return c.text(`db-pwd: ${c.env.COCKROACH_CONN_STR}`)
+  startTime = (new Date()).toISOString()
+  const db = new Client({connectionString: c.env.COCKROACH_CONN_STR})
+  await db.connect()
+  let row = (await db.query('select current_timestamp tstamp')).rows[0]
+  const dbTime = row.tstamp.toISOString()
+
+  db.close();
+  return c.json({startTime,
+                dbTime,
+                endTime: (new Date().toISOString()
+  })
    })
 
 export default app
