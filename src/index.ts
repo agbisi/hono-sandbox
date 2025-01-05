a00d479c85d31aefc054c83c88c06facc560bb3f
ajuala <kamalustudio@yandex.com>
2025-01-05 23:44:19 +0100

    test cleared

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,7 @@ import { Client } from 'pg'
 const app = new Hono()
 
 app.get('/',  (c) => {
-
-  const startTime = (new Date()).toISOString()
-  const connstr = c.env.COCKROACH_CONN_STR
-  const db = new Client({connectionString:  connstr })
-  let r = db.connect()
-    .then(()=> ( db.query('select current_timestamp tstamp')).rows[0].tstamp.toISOString())
-      .then( (dbTime)=> c.json({
-    startTime,
-    dbTime,
-    message: 'Hello Hono',
-    endTime: (new Date()).toISOString(),
-  }))
-
-  return r
-
+  return c.text(`pwd: ${c.env.COCKROACH_CONN_STR}`)
    })
 
 export default app
