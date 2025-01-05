f3ca926aec931e7ffa6ddc09ed68f62a0888fac9
ajuala <kamalustudio@yandex.com>
2025-01-05 13:40:47 +0100

    updated src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.json({
+    message: 'Hello Hono',
+    date: (new Date().toISOString(),
+  })
 })
 
 export default app
