ad75be3fc4fd2a09dc32750094dd08cedbf3d38d
ajuala <kamalustudio@yandex.com>
2025-01-05 23:55:56 +0100

    test secret

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { Client } from 'pg'
 const app = new Hono()
 
 app.get('/',  (c) => {
-  return c.text(`pwd: ${c.env.COCKROACH_CONN_STR}`)
+  return c.text(`db_pwd: ${c.env.COCKROACH_CONN_STR}`)
    })
 
 export default app
