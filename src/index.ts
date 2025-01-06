559db3f686e614bebb0faca879575814533a0c8b
ajuala <kamalustudio@yandex.com>
2025-01-06 18:16:49 +0100

    changes purged

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,9 @@ import { Client } from 'pg'
 
 const app = new Hono()
 
-app.get('/',  async (c) => {
-  const startTime = (new Date()).toISOString()
-  console.log(startTome)
-  const db = new Client({connectionString: c.env.COCKROACH_CONN_STR})
-  await db.connect()
-  let row = (await db.query('select current_timestamp tstamp')).rows[0]
-  const dbTime = row.tstamp.toISOString()
-
-  await db.end()
-  return c.json({startTime,
-                dbTime,
-                endTime: (new Date()).toISOString()
+app.get('/',   (c) => {
+  return c.json({
+    message: 'Hello Hono'
   })
    })
 
