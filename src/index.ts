0cc36f0d6736b834327402901f2acafa00796f22
ajuala <kamalustudio@yandex.com>
2025-01-06 17:54:57 +0100

    minor bug fix

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { Client } from 'pg'
 const app = new Hono()
 
 app.get('/',  async (c) => {
-  startTime = (new Date()).toISOString()
+  const startTime = (new Date()).toISOString()
   const db = new Client({connectionString: c.env.COCKROACH_CONN_STR})
   await db.connect()
   let row = (await db.query('select current_timestamp tstamp')).rows[0]
