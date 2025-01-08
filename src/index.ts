a441c913f29e0d6095dae191ee36078bd169003a
ajuala <kamalustudio@yandex.com>
2025-01-08 23:37:48 +0100

    test for async 16

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import { Client } from 'pg'
 const app = new Hono()
 
 app.get('/', async (c) => {
-  console.log('running')
+  console.log('My Hono app has b-gone')
   let svDate = new Date().toISOString()
   let apiKey = c.env.COCKROACH_CONN_STR
 
   const db = new Client({
     connectionString: apiKey,
-    statement_tomeout: 30000,
+    statement_timeout: 30000,
     connectionTimeoutMillis: 30000,
     lock_timeout: 30000,
     query_timeout: 30000,
