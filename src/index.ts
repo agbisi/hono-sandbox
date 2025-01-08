4e79a76a90ef8c2be5f1fa169cee65d2953f12a4
ajuala <kamalustudio@yandex.com>
2025-01-08 16:06:22 +0100

    test for async 2

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ import { Client } from 'pg'
 const app = new Hono()
 
 app.get('/', async (c) => {
+  let apiKey = c.env.COCKROACH_CONN_STR
   let sum = await (3 + 7)
   return c.json({
     message: 'Hello Hono',
-    sum
+    sum,
+    ky: apiKey.slice(0, 5)
   })
    })
 
