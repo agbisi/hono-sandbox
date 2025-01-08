b7fe10ebd1d26cb4362089b3a061c105fb49adfa
ajuala <kamalustudio@yandex.com>
2025-01-08 16:21:48 +0100

    test for async 3

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,25 @@ import { Client } from 'pg'
 const app = new Hono()
 
 app.get('/', async (c) => {
+  let svDate = new Date().toISOString()
   let apiKey = c.env.COCKROACH_CONN_STR
-  let sum = await (3 + 7)
+
+  const db = new Client({connectionString: apiKey})
+
+  try {
+    let roes = await db.query('select cuurent_timestamp tstamp')
+    let dbDate = rows[0].tstamp.toISOString()
+    return c.json({
+      svDate,
+      dbDate
+    })
+  } catch (err) {
+    console.log(err)
+    return c.text('Unknown error!')
+  }
+
   return c.json({
     message: 'Hello Hono',
-    sum,
     ky: apiKey.slice(0, 5)
   })
    })
