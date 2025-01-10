05e867ccd9086bf496d767c914b358b1d4462b46
ajuala <kamalustudio@yandex.com>
2025-01-10 10:08:14 +0100

    Another shot at DB testing

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,26 @@ import { Client } from 'pg'
 const app = new Hono()
 
 app.get('/', async (c) => {
-  return c.text('reset')
+  const serverTime = new Date().toISOString()
+
+  const db = new Client({
+    connectionString: c.env.COCKROACH_CONN_STR,
+    ssl: {
+      rejectUnauthorized: false,
+      ca: c.env.CERT,
+    }
+  })
+
+  await db.connect()
+  let row = (await db.query('SELECT NOW() tstamp')).rows[0]
+  const dbTime = row.tstamp.toString()
+  await db.end()
+
+  return c.json({
+    serverTime,
+    dbTime,
+  })
+
    })
 
 export default app
